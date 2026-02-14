6f93eab08011c2db41a7d5dcd89e0ea545cd39c8
Delusionaut <w.anderson92@gmail.com>
2026-02-14 02:40:47 -0800

    Update Kotlin to 1.7.20 and KSP to 1.7.20-1.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "7.0.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
-    id("com.google.devtools.ksp") version "1.6.21-1.0.5" apply false
+    id("com.google.devtools.ksp") version "1.7.20-1.0.8" apply false
 }
