80405314093a04e3760017942908a72cfe71e831
Delusionaut <w.anderson92@gmail.com>
2026-02-14 05:00:58 -0800

    Test simple UI

---
+++ app/src/main/java/com/kenotracker/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.kenotracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(modifier = Modifier.fillMaxSize()) {
+                // This should show a plain screen
+            }
+        }
+    }
+}
