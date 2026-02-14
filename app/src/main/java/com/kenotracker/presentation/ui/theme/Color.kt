f99bbe3b915a2618ef5806a3d6a9388ddcfb7b5e
Delusionaut <111107894+Delusionaut@users.noreply.github.com>
2026-02-13 23:23:40 -0800

    Add Color.kt for theme color definitions

---
+++ app/src/main/java/com/kenotracker/presentation/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.kenotracker.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary colors
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// App specific colors
+val KenoBackground = Color(0xFF1A1A2E)
+val KenoSurface = Color(0xFF252540)
+val KenoPrimary = Color(0xFF6200EE)
+val KenoSecondary = Color(0xFFFFB300)
+val KenoSelected = Color(0xFF00E676)
+val KenoUnselected = Color(0xFF4A4A6A)
+val KenoText = Color(0xFFFFFFFF)
+val KenoTextSecondary = Color(0xFFB0B0B0)
+
+val HotColor = Color(0xFFFF5252)
+val ColdColor = Color(0xFF448AFF)
+val TrendColor = Color(0xFFFFD740)
